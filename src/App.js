3649ac308e19f1554a4a6fa491d3251b6a091fc1
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-17 23:04:42 +0100

    set up boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!!! This is a wave portal.
+    </div>
+  );
+}
+
+export default App;
